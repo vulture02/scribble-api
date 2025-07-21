9b9b30ff5cec9c8e4a7a12d414ae02e66962f0b1
vulture02 <amithp0210@gmail.com>
2025-07-21 10:57:34 +0530

    connection to data base file

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,9 +2,10 @@ import mongoose from "mongoose";
 
 export const connectDB=async()=>{
     try{
-       await mongoose.connect("mongodb+srv://amithp460:9MEqJdDL58pWMzol@cluster0.tyfmk5a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+       await mongoose.connect(process.env.MONGO_URI)
          console.log("MongoDB connected successfully");
     }catch(error){
         console.error("MongoDB connection failed:", error.message);
+        process.exit(1); // Exit the process with failure
     }
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import notesRoutes from './routes/notesRoutes.js';
 import { connectDB } from './config/db.js';
+import dotenv from 'dotenv';
+dotenv.config();
 const app= express();
 
 connectDB();
