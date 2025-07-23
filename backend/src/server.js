5054a689933e0dd880dcd6eb8bed5b724b75ef99
vulture02 <amithp0210@gmail.com>
2025-07-23 15:54:29 +0530

    deploye

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,29 +1,42 @@
-import express from 'express';
-import notesRoutes from './routes/notesRoutes.js';
-import { connectDB } from './config/db.js';
-import dotenv from 'dotenv';
-import ratelimiter from './middleware/rateLimiter.js';
-import cors from 'cors';
+import express from "express";
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+import ratelimiter from "./middleware/rateLimiter.js";
+import cors from "cors";
+import path from "path";
 dotenv.config();
-const app= express();
+const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
+if(process.env.NODE_ENV !== "production") {
+    app.use(
+  cors({
+    origin: "http://localhost:5173", // Adjust this to your frontend UR
+  })
+)
 
-app.use(cors({
-    origin: 'http://localhost:5173', // Adjust this to your frontend UR
-}))
-app.use(express.json()); 
+}
+;
+app.use(express.json());
 app.use(ratelimiter);
 
 // app.use((req, res, next) => {
 //     console.log("method")
 //     next();
 // })
-app.use("/api/notes",notesRoutes);
+app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
-    app.listen(PORT,()=>{
+  app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+  });
 });
-
-})
-
